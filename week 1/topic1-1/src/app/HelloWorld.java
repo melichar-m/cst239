e0a1e1c70be2ca42c9344f559fbdc4d6d09aaa0c
Michael Melichar <35645069+bynarywow@users.noreply.github.com>
2022-07-10 18:59:16 -0700

    final
    activity 1-1/2 commit

--- week 1/topic1-1/src/app/HelloWorld.java
+++ week 1/topic1-1/src/app/HelloWorld.java
@@ -2,6 +2,10 @@ package app;
 
 public class HelloWorld 
 {
+	/**
+	 * Prints an entered string, presumably the user's name in chat after "Hello, my name is"
+	 * @param name
+	 */
 	private void sayHello(String name)
 	{
 		System.out.println("Hello, my name is " + name);
