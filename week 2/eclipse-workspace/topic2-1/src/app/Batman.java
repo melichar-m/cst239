503f3c193a13d84721af9f2cb7714834a5e9d69c
bynarywow <michael.melichar2@gmail.com>
2022-07-15 07:58:53 -0700

    empty

--- week 2/eclipse-workspace/topic2-1/src/app/Batman.java
+++
@@ -1,9 +0,0 @@
-package app;
-
-public class Batman extends SuperHero
-{
-	public Batman(int health)
-	{
-		super("Batman",health);
-	}
-}
--- week 2/eclipse-workspace/topic2-1/src/app/Superman.java
+++
@@ -1,9 +0,0 @@
-package app;
-
-public class Superman extends SuperHero
-{
-	public Superman(int health)
-	{
-		super("Superman",health);
-	}
-}
